3b42dcf7f33e4b501bc46011b76942a44b2ceef5
RethinkWizard <Rethink-Wizard@protonmail.com>
2026-08-06 18:45:57 +0200

    Last Sync: 2026-08-06 18:45 (Mobile)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "de.dragonschain.offgridmesh"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1-prototyp"
+        versionCode = 2
+        versionName = "0.2-prototyp"
     }
 
     buildTypes {
